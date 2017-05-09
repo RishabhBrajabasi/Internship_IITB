d847182e3b1a4083b9dfb25660146e869d094d38
Rishabh <rishabhbrajabasi@gmail.com>
2017-05-09 12:15:30 +0530

    Working model 1.

--- Envelope.py
+++ Envelope.py
@@ -5,70 +5,6 @@ from scipy.signal import butter, lfilter
 
 audio_file = 'F:\Projects\Active Projects\Project Intern_IITB\Vowel Evaluation PE V4\Analyze\Vowel_Evaluation_V4_I2\\17.wav'
 
-def peaks(st_energy_peak, threshold_peak):
-    peak_f = []
-    location_peak_f = []
-    for p in range(len(st_energy_peak)):
-        if p == 0:
-            peak_f.append(0)
-        elif p == len(st_energy_peak) - 1:
-            peak_f.append(0)
-        else:
-            if st_energy_peak[p] > st_energy_peak[p + 1] and st_energy_peak[p] > st_energy_peak[p - 1] and st_energy_peak[p] >= threshold_peak:
-                peak_f.append(st_energy_peak[p])
-                location_peak_f.append(p)
-            else:
-                peak_f.append(0)
-    return peak_f, location_peak_f
-
-def valleys(st_energy_valley, threshold_valley):
-    valley_f = []
-    location_valley_f = []
-    for v in range(len(st_energy_valley)):
-        if v == 0:
-            if st_energy_valley[v] < st_energy_valley[v + 1] and st_energy_valley[v] < threshold_valley:
-                valley_f.append(st_energy_valley[v])
-                location_valley_f.append(v)
-            else:
-                valley_f.append(0)
-        elif v == len(st_energy_valley) - 1:
-            if st_energy_valley[v] < st_energy_valley[v - 1] and st_energy_valley[v] < threshold_valley:
-                valley_f.append(st_energy_valley[v])
-                location_valley_f.append(v)
-            else:
-                valley_f.append(0)
-        else:
-            if st_energy_valley[v] < st_energy_valley[v + 1] and st_energy_valley[v] < st_energy_valley[v - 1] and st_energy_valley[v] < threshold_valley:
-                valley_f.append(st_energy_valley[v])
-                location_valley_f.append(v)
-            else:
-                valley_f.append(0)
-    return valley_f, location_valley_f
-
-def peak_valley_elimination(location_peak_pve, location_valley_pve):
-    location_combined = location_peak_pve + location_valley_pve
-    location_combined.sort()
-    peak_valley_pair = [[]]
-    peak_valley_pair_value = [[]]
-
-    for ele in range(len(location_combined)):
-        if location_combined[ele] in location_peak_pve:
-            peak_valley_pair.append([location_combined[ele - 1], location_combined[ele], location_combined[ele + 1]])
-            peak_valley_pair_value.append([d1[location_combined[ele - 1]], d1[location_combined[ele]], d1[location_combined[ele + 1]]])
-
-    peak_valley_pair.pop(0)
-    peak_valley_pair_value.pop(0)
-    # for ele in range(len(peak_valley_pair)-1):
-    #     if peak_valley_pair[ele][2] != peak_valley_pair[ele+1][0]:
-    #         if peak_valley_pair[ele][2] > peak_valley_pair[ele+1][0]:
-    #             peak_valley_pair[ele+1][0] = peak_valley_pair[ele][2]
-    #             peak_valley_pair_value[ele+1][0] = peak_valley_pair_value[ele][2]
-    #         else:
-    #             peak_valley_pair[ele][2] = peak_valley_pair[ele + 1][0]
-    #             peak_valley_pair_value[ele][2] = peak_valley_pair_value[ele + 1][0]
-
-    return peak_valley_pair, peak_valley_pair_value
-
 def butter_bandpass(highcut, fs, order=5):
     nyq = 0.5 * fs
     high = highcut / nyq
@@ -92,7 +28,7 @@ batman = data
 max_batman = max(batman)
 for k in range(len(batman)):
     batman[k] = batman[k]/max_batman
-#----------------------------------------------------------------------------------------------------------------------#
+
 data1 = []
 for i in range(len(data)):
     if data[i] < 0:
@@ -113,49 +49,90 @@ for k in range(len(d1)):
     d1[k] = d1[k]/max1
 d1 = d1.tolist()
 
-peak, location_peak = peaks(d1, 0.2)
-valley, location_valley = valleys(d1, 0.8)
+st_energy_peak = d1
+threshold_peak = 0.2
 
-peak_valley, peak_valley_value = peak_valley_elimination(location_peak, location_valley)
+peak_f = []
+location_peak_f = []
+for p in range(len(st_energy_peak)):
+    if p == 0:
+        peak_f.append(0)
+    elif p == len(st_energy_peak) - 1:
+        peak_f.append(0)
+    else:
+        if st_energy_peak[p] > st_energy_peak[p + 1] and st_energy_peak[p] > st_energy_peak[p - 1] and st_energy_peak[p] >= threshold_peak:
+            peak_f.append(st_energy_peak[p])
+            location_peak_f.append(p)
+        else:
+            peak_f.append(0)
 
 the_list = [[]]
 the_list_value = [[]]
-for lines in range(len(peak_valley)):
-    value = d1[peak_valley[lines][1]]*0.85
-    for ele in range(peak_valley[lines][1], -1, -1):
+count = 0
+for lines in range(len(location_peak_f)):
+    value = d1[location_peak_f[lines]]*0.85
+    for ele in range(location_peak_f[lines], -1, -1):
         if d1[ele] < value:
             flash_1 = ele
             break
-    for ele in range(peak_valley[lines][1], len(d1), 1):
+    for ele in range(location_peak_f[lines], len(d1), 1):
         if d1[ele] < value:
             flash_2 = ele
             break
-    the_list.append([peak_valley[lines][0], flash_1, peak_valley[lines][1], flash_2, peak_valley[lines][2]])
-    the_list_value.append([peak_valley[lines][0], flash_1, peak_valley[lines][1], flash_2, peak_valley[lines][2],
-                           d1[peak_valley[lines][0]], d1[peak_valley[lines][1]], d1[peak_valley[lines][2]]])
+    the_list.append([flash_1, location_peak_f[lines], flash_2, count])
 
+    the_list_value.append([flash_1, location_peak_f[lines], flash_2, count, d1[location_peak_f[lines]]])
+    count += 1
+the_list.pop(0)
 the_list_value.pop(0)
-s = sorted(the_list_value, key=lambda student: student[6], reverse=1)
+s = sorted(the_list_value, key=lambda student: student[4], reverse=1)
 
-a = [[]]
+remove = []
 for l in range(len(s)):
     for k in range(l+1, len(s)):
-        if s[l][1] < s[k][2] < s[l][3]:
-            a.append([s[k][0], s[k][1], s[k][2], s[k][3], s[k][4], s[k][5], s[k][6], s[k][7]])
-a.pop(0)
+        if s[l][0] < s[k][1] < s[l][2] and s[k][3] not in remove:
+            remove.append(s[k][3])
 
-new = [[]]
-for gl in the_list_value:
-    if gl not in a:
-        new.append(gl)
-new.pop(0)
+superman = [[]]
+for ww in the_list_value:
+    if ww[3] not in remove:
+        superman.append(ww)
+superman.pop(0)
 
-peak_valley_new = [[]]
-for lines in range(len(new)):
-    plt.vlines(new[lines][0], min(d1), d1[new[lines][0]], 'green')
-    plt.vlines(new[lines][2], d1[new[lines][2]]*0.85, d1[new[lines][2]], 'black')
-    plt.vlines(new[lines][4], min(d1), d1[new[lines][4]], 'green')
-    plt.hlines(d1[new[lines][2]]*0.85, new[lines][1], new[lines][3], 'red', linewidth='2.0')
+cyborg = [[]]
+valley = [0]
+for arrow in range(len(superman)-1):
+    minimum = min(d1[superman[arrow][1]:superman[arrow+1][1]])
+    location_minimum = d1.index(minimum)
+    valley.append(location_minimum)
+valley.append(len(d1)-1)
+
+fresh_count = 0
+for cc in range(len(superman)):
+    cyborg.append([valley[cc], superman[cc][0], superman[cc][1], superman[cc][2], valley[cc+1], fresh_count])
+    fresh_count += 1
+cyborg.pop(0)
+
+remove[:] = []
+for cc in cyborg:
+    if cc[3] > cc[4] or cc[0] > cc[1]:
+        remove.append(cc[5])
+
+star_fire = []
+for sf in cyborg:
+    if sf[5] not in remove:
+        star_fire.append(sf[2])
+
+val = [0]
+for v in range(len(star_fire)-1):
+    minimum = min(d1[star_fire[v]:star_fire[v+1]])
+    val.append(d1.index(minimum))
+val.append(len(d1)-1)
+
+for p in range(len(star_fire)):
+    plt.vlines(star_fire[p], 0, d1[star_fire[p]], 'red', linewidth='2.0')
+for v in range(len(val)):
+    plt.vlines(val[v], 0, d1[val[v]], 'green', linewidth='2.0')
 
 plt.plot(d1, 'black', label='Envelope', linewidth='2.0')
 plt.show()
