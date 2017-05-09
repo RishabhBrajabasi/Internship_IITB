8dd173985cad94edfb00c2bbe9ed9d45b2853267
Rishabh <rishabhbrajabasi@gmail.com>
2017-05-09 10:23:54 +0530

    If things hit the fan, revert back to this.

--- Envelope.py
+++ Envelope.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, lfilter
 
-audio_file = 'F:\Projects\Active Projects\Project Intern_IITB\Vowel Evaluation PE V4\Analyze\Vowel_Evaluation_V4_I2\\6.wav'
+audio_file = 'F:\Projects\Active Projects\Project Intern_IITB\Vowel Evaluation PE V4\Analyze\Vowel_Evaluation_V4_I2\\17.wav'
 
 def peaks(st_energy_peak, threshold_peak):
     peak_f = []
@@ -20,7 +20,7 @@ def peaks(st_energy_peak, threshold_peak):
             else:
                 peak_f.append(0)
     return peak_f, location_peak_f
-#----------------------------------------------------------------------------------------------------------------------#
+
 def valleys(st_energy_valley, threshold_valley):
     valley_f = []
     location_valley_f = []
@@ -44,7 +44,7 @@ def valleys(st_energy_valley, threshold_valley):
             else:
                 valley_f.append(0)
     return valley_f, location_valley_f
-#----------------------------------------------------------------------------------------------------------------------#
+
 def peak_valley_elimination(location_peak_pve, location_valley_pve):
     location_combined = location_peak_pve + location_valley_pve
     location_combined.sort()
@@ -55,26 +55,36 @@ def peak_valley_elimination(location_peak_pve, location_valley_pve):
         if location_combined[ele] in location_peak_pve:
             peak_valley_pair.append([location_combined[ele - 1], location_combined[ele], location_combined[ele + 1]])
             peak_valley_pair_value.append([d1[location_combined[ele - 1]], d1[location_combined[ele]], d1[location_combined[ele + 1]]])
+
     peak_valley_pair.pop(0)
     peak_valley_pair_value.pop(0)
+    # for ele in range(len(peak_valley_pair)-1):
+    #     if peak_valley_pair[ele][2] != peak_valley_pair[ele+1][0]:
+    #         if peak_valley_pair[ele][2] > peak_valley_pair[ele+1][0]:
+    #             peak_valley_pair[ele+1][0] = peak_valley_pair[ele][2]
+    #             peak_valley_pair_value[ele+1][0] = peak_valley_pair_value[ele][2]
+    #         else:
+    #             peak_valley_pair[ele][2] = peak_valley_pair[ele + 1][0]
+    #             peak_valley_pair_value[ele][2] = peak_valley_pair_value[ele + 1][0]
+
     return peak_valley_pair, peak_valley_pair_value
-#----------------------------------------------------------------------------------------------------------------------#
+
 def butter_bandpass(highcut, fs, order=5):
     nyq = 0.5 * fs
     high = highcut / nyq
     b, a = butter(order, high, btype='low')
     return b, a
-#----------------------------------------------------------------------------------------------------------------------#
+
 def butter_bandpass_filter(data, highcut, fs, order=5):
     b, a = butter_bandpass(highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
-#----------------------------------------------------------------------------------------------------------------------#
+
 def moving_average(interval, window_size):
     window = np.ones(int(window_size))/float(window_size)
     plt.show()
     return np.convolve(interval, window, 'same')
-#----------------------------------------------------------------------------------------------------------------------#
+
 fs, data = wavfile.read(audio_file)
 data = data / float(2 ** 15)
 x_values = np.arange(0, len(data), 1) / float(fs)
@@ -105,15 +115,13 @@ d1 = d1.tolist()
 
 peak, location_peak = peaks(d1, 0.2)
 valley, location_valley = valleys(d1, 0.8)
+
 peak_valley, peak_valley_value = peak_valley_elimination(location_peak, location_valley)
 
+the_list = [[]]
+the_list_value = [[]]
 for lines in range(len(peak_valley)):
-    plt.vlines(peak_valley[lines][0], min(d1), d1[peak_valley[lines][0]], 'green')
-    plt.vlines(peak_valley[lines][1], min(d1), d1[peak_valley[lines][1]], 'black')
-    plt.vlines(peak_valley[lines][2], min(d1), d1[peak_valley[lines][2]], 'green')
-
-for lines in range(len(peak_valley)):
-    value = d1[peak_valley[lines][1]]*0.8
+    value = d1[peak_valley[lines][1]]*0.85
     for ele in range(peak_valley[lines][1], -1, -1):
         if d1[ele] < value:
             flash_1 = ele
@@ -122,6 +130,32 @@ for lines in range(len(peak_valley)):
         if d1[ele] < value:
             flash_2 = ele
             break
-    plt.hlines(d1[peak_valley[lines][1]]*0.8, flash_1, flash_2, 'red', linewidth='2.0')
+    the_list.append([peak_valley[lines][0], flash_1, peak_valley[lines][1], flash_2, peak_valley[lines][2]])
+    the_list_value.append([peak_valley[lines][0], flash_1, peak_valley[lines][1], flash_2, peak_valley[lines][2],
+                           d1[peak_valley[lines][0]], d1[peak_valley[lines][1]], d1[peak_valley[lines][2]]])
+
+the_list_value.pop(0)
+s = sorted(the_list_value, key=lambda student: student[6], reverse=1)
+
+a = [[]]
+for l in range(len(s)):
+    for k in range(l+1, len(s)):
+        if s[l][1] < s[k][2] < s[l][3]:
+            a.append([s[k][0], s[k][1], s[k][2], s[k][3], s[k][4], s[k][5], s[k][6], s[k][7]])
+a.pop(0)
+
+new = [[]]
+for gl in the_list_value:
+    if gl not in a:
+        new.append(gl)
+new.pop(0)
+
+peak_valley_new = [[]]
+for lines in range(len(new)):
+    plt.vlines(new[lines][0], min(d1), d1[new[lines][0]], 'green')
+    plt.vlines(new[lines][2], d1[new[lines][2]]*0.85, d1[new[lines][2]], 'black')
+    plt.vlines(new[lines][4], min(d1), d1[new[lines][4]], 'green')
+    plt.hlines(d1[new[lines][2]]*0.85, new[lines][1], new[lines][3], 'red', linewidth='2.0')
+
 plt.plot(d1, 'black', label='Envelope', linewidth='2.0')
 plt.show()
